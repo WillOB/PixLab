ea6b38e7ba32cb55147c688d202beca216ce3628
William Patrick O Brien <obrienwp@dcsdk12.org>
2017-02-13 12:46:02 -0700

    Exercise 1

--- src/Picture.java
+++ src/Picture.java
@@ -249,8 +249,11 @@ public class Picture extends SimplePicture {
 				leftPixel = pixels[row][col];
 				rightPixel = pixels[row][mirrorPoint - col + mirrorPoint];
 				rightPixel.setColor(leftPixel.getColor());
+				
+				count++;
 			}
 		}
+		System.out.println(count);
 	}
 
 	/**
